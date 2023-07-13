eaf795ceb22e6a12ca697e9320ff906b2022e5bd
Армен Погосян <armen-pogosyan@mail.ru>
2023-07-13 13:50:11 +0300

    1

--- models/user.js
+++ models/user.js
@@ -7,8 +7,6 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    minlength: [2, 'Минимальная длина поля 2 символа'],
-    maxlength: 30,
   },
   email: {
     type: String,
