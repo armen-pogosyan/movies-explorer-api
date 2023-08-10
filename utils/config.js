1d1e8027eaaa5819b6ad129209fff6b9955d61e6
Армен Погосян <armen-pogosyan@mail.ru>
2023-08-11 02:34:05 +0300

    Диплом_1.5

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
-const { DB_CONN = 'mongodb://127.0.0.1:27017/bitfilmsdb', PORT = 3001 } = process.env;
+const { DB_CONN = 'mongodb://127.0.0.1:27017/bitfilmsdb', PORT = 3000 } = process.env;
 
 module.exports = {
   DB_CONN,
