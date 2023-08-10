13e2bc6955ff7f207726464f93b6a4a77a7da230
Армен Погосян <armen-pogosyan@mail.ru>
2023-08-11 02:31:59 +0300

    Диплом_1.4

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,7 +1,9 @@
 module.exports = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
-  res.status(statusCode).send(
-    { message: statusCode === 500 ? 'На сервере произошла ошибка' : message },
-  );
+  // res.status(statusCode).send(
+  //   { message: statusCode === 500 ? 'На сервере произошла ошибка' : message },
+  // );
+  res.status(statusCode);
+  res.json({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
   next();
 };
